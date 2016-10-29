390ad2d8c995748b108554b6bffa5e44a90bf7bd
George <loaizageorge@gmail.com>
2016-10-28 21:12:42 -0400

    Fix mobile scroll offset

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
            if (target.length) {
              $('html,body').animate({
-                 scrollTop: target.offset().top-20
+                 scrollTop: target.offset().top-28
             }, 1000);
             return false;
         }
